3f99357b6771907151989ce20c2028e3e424fbd1
Simranjeet Singh <sjsingh9090@gmail.com>
2020-01-20 08:00:46 +0530

    Boilerplate Code

---
+++ app/src/main/java/com/sjsingh101/resumebuddy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sjsingh101.resumebuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
